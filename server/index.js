6418467a03525668315d3017731155691e941ac0
Andrea Lo Cicero <jpgflipper@gmail.com>
2023-11-19 22:54:49 +0100

    FIX CORS

--- server/index.js
+++ server/index.js
@@ -7,8 +7,15 @@ const id = process.env.SCOREBOARD_ID;
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors({ origin: `https://scoreboard-justanotherwebdev.vercel.app` }));
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', `${process.env.FRONTEND_URL}`);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 
 
 mongoose.connect(process.env.MONGODB_URI)
