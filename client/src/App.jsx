9b3b63802a8a22055f5d1c4b202defc39bf468d0
Andrea Lo Cicero <jpgflipper@gmail.com>
2023-11-19 18:55:37 +0100

    Fixed base route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Scoreboard from "./components/Scoreboard"
+import Home from "./components/Home"
 import "./index.css"
 
 const App = () => {
