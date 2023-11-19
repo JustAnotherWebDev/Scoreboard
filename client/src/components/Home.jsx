9c2a15e5495303d3f33426a7c9eb636c03ce758c
Andrea Lo Cicero <jpgflipper@gmail.com>
2023-11-19 19:25:50 +0100

    Test

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom"
 const Home = () => {
-  return <div>Hello World</div>
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Link to="scoreboard">Scoreboard</Link>
+    </>
+  )
 }
 
 export default Home
