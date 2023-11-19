8b5b099799c245fa62f067e12140bfd3c2acf3b5
Andrea Lo Cicero <jpgflipper@gmail.com>
2023-11-19 23:23:39 +0100

    Ditch cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', `${process.env.FRONTEND_URL}`);
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
